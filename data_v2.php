d374e40f7fb9722ff48647808ccab1d0a6dfc82f
Ben Owen <ben.owen3183@gmail.com>
2021-12-03 12:54:31 +0000

    added data version 2

---
+++ data_v2.php
@@ -0,0 +1,29 @@
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "lol12345";
+$dbName = "solar";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ return;
+}
+
+$type = $_GET['type'];
+$sql = "SELECT value FROM data WHERE type="."'".$type."'";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+echo "value: ".$row["value"];
+
+$conn->close();
+
+?>
