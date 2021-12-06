3178182e97c2a5c9b5ef99e0c4af820d31ef0f34
Ben Owen <ben.owen3183@gmail.com>
2021-12-06 14:20:00 +0000

    fixed script eror

--- update.sh
+++ update.sh
@@ -11,5 +11,5 @@ white='\e[0;37m'
 echo "\nrun the commands: "
 echo -e "\t${yellow}sudo systemctl status nginx"
 echo -e "${white}and"
-echo -e "\t${yellow}sudo systemctl status nginx"
+echo -e "\t${yellow}sudo systemctl status solar_scraper.service"
 echo -e "${white}to ensure both are active"
