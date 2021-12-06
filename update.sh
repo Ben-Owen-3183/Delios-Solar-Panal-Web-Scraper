8d3a8c35fc69a649d8c6026a146afaf16a8938b8
Ben Owen <ben.owen3183@gmail.com>
2021-12-06 14:04:13 +0000

    added update script

--- update.sh
+++ update.sh
@@ -1,3 +1,15 @@
 git stash
-git pull
+git pull 
 sudo systemctl restart nginx
+sudo systemctl restart solar_scraper.service
+
+
+
+yellow='\e[0;33m'
+white='\e[0;37m'
+
+echo "\nrun the commands: "
+echo -e "\t${yellow}sudo systemctl status nginx"
+echo -e "${white}and"
+echo -e "\t${yellow}sudo systemctl status nginx"
+echo -e "${white}to ensure both are active"
